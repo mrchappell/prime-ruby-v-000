92ce5162fe3e15c5b281491c07038e78ee2fdd1f
mrchappell <michael.r.chappell@gmail.com>
2019-08-21 19:25:50 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 0 || integer == 0 || integer == 1
+      return false
+    else
+      (2..integer-1).to_a.all? do |prime_check|
+        integer % prime_check != 0
+    end
+  end
+end
+
+
+puts prime? (14)
